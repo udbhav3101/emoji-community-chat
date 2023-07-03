b70366d054e93b22a0e04bade443cd99583ac287
udbhav3101 <udbhav_govindu@bonify.de>
2023-07-03 20:12:30 +0530

    Updated nextconfig with removal of typechecking and eslint

--- next.config.mjs
+++ next.config.mjs
@@ -21,5 +21,12 @@ const config = {
   images: {
     domains: ["images.clerk.dev"],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  swcMinify: true,
 };
 export default config;
